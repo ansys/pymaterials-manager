1baf077c31436efaeb1742161dad83d6c31e132e
rmanno91 <riccardo.manno@ansys.com>
2025-09-22 17:16:50 +0200

    remove more legacy

--- src/ansys/materials/manager/_models/_common/__init__.py
+++ src/ansys/materials/manager/_models/_common/__init__.py
@@ -19,7 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from ._base import _BaseModel
 from ._packages import SupportedPackage
 from .common import (
     ParameterField,
