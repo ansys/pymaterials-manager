32210b4659d6c1e0c8876a63164d4e45d5f0d726
rmanno91 <riccardo.manno@ansys.com>
2025-10-15 14:06:10 +0200

    add test

---
+++ tests/ls_dyna/test_elasticity_lsdyna_write.py
@@ -0,0 +1,67 @@
+# Copyright (C) 2022 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from ansys.units import Quantity
+
+from ansys.materials.manager._models._material_models.density import Density
+from ansys.materials.manager._models._material_models.elasticity_isotropic import (
+    ElasticityIsotropic,
+)
+from ansys.materials.manager._models.material import Material
+from ansys.materials.manager.util.ls_dyna.writer_ls_dyna import WriterLsDyna
+
+
+def test_write_constant_elasticity_isotropic():
+    elasticity = ElasticityIsotropic(
+        youngs_modulus=Quantity(value=[1000000], units="Pa"),
+        poissons_ratio=Quantity(value=[0.3], units=""),
+    )
+
+    material = Material(name="Linear Elastic", models=[elasticity])
+
+    material_id = 1
+    lsdyna_material = WriterLsDyna().write_material(material, material_id)
+    assert len(lsdyna_material) == 1
+    assert lsdyna_material[0].e == elasticity.youngs_modulus.value[0]
+    assert lsdyna_material[0].pr == elasticity.poissons_ratio.value[0]
+    assert lsdyna_material[0].mid == material_id
+
+
+def test_write_constant_elasticity_isotropic_with_density():
+    density = Density(
+        density=Quantity(value=[1.34], units="kg m^-3"),
+    )
+
+    elasticity = ElasticityIsotropic(
+        youngs_modulus=Quantity(value=[1000000], units="Pa"),
+        poissons_ratio=Quantity(value=[0.3], units=""),
+    )
+
+    material = Material(name="Linear Elastic", models=[density, elasticity])
+
+    material_id = 2
+    lsdyna_material = WriterLsDyna().write_material(material, material_id)
+    assert len(lsdyna_material) == 1
+    assert lsdyna_material[0].e == elasticity.youngs_modulus.value[0]
+    assert lsdyna_material[0].pr == elasticity.poissons_ratio.value[0]
+    assert lsdyna_material[0].ro == density.density.value[0]
+    assert lsdyna_material[0].mid == material_id
