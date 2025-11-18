052fb0866631b8c37b4018d3505144f34ed3c45b
rmanno91 <riccardo.manno@ansys.com>
2025-11-18 18:33:25 +0100

    fix index

--- tests/material_manger/test_material_manager.py
+++ tests/material_manger/test_material_manager.py
@@ -45,7 +45,7 @@ def test_read_matml_write_apdl(mapdl):
     material_manager = MaterialManager()
     material_manager.read_from_matml(ELASTICITY_MATML)
     material = material_manager.get_material("Isotropic Test Material")
-    isotropic_elasticity = material.models[1]
+    isotropic_elasticity = material.models[0]
     assert isotropic_elasticity.name == "Elasticity"
     assert isotropic_elasticity.model_qualifiers[0].name == "Behavior"
     assert isotropic_elasticity.model_qualifiers[0].value == "Isotropic"
